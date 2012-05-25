97c3ea5d3b7abecff4df9fb3eb575d3fa24903df
Abhinav Bommakanti <Abhinav.Bommkanti@ca.flextronics.com>
2012-05-25 12:35:04 -0400

    Changes stars to pluses

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
@@ -2,8 +2,8 @@ package com.wakaleo.gameoflife.domain;
 
 import static com.wakaleo.gameoflife.domain.Cell.DEAD_CELL;
 import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
 
 import org.junit.Test
 
